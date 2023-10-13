c1b1ecf22cf0bfcab6e07823f78cf8855cab128b
root <NyangoyaG>
2023-10-13 10:04:53 +0000

    Final correction

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,21 @@
+#!/usr/bin/python3
+'''A module for working with lockboxes.
+'''
+
+
 def canUnlockAll(boxes):
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
     n = len(boxes)
-    visited = [False] * n
-    stack = [0]  # Start with the first box (box 0)
-    visited[0] = True
-
-    while stack:
-        current_box = stack.pop()
-        for key in boxes[current_box]:
-            if 0 <= key < n and not visited[key]:
-                stack.append(key)
-                visited[key] = True
-
-    return all(visited)
-
-# Example usage:
-boxes = [[1], [2], [3], []]
-print(canUnlockAll(boxes))  # Output: True
-
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
