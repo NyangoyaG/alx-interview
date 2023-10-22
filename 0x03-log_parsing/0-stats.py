22b62c99ce62732e1d4f04ac41d308d660f5ebdb
root <NyangoyaG>
2023-10-22 08:45:06 +0000

    Final update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,50 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """
-Log parsing
+log parsing
 """
+
 import sys
-import signal
+import re
 
-# Define the status codes to track
-status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 
-# Initialize variables to store statistics
-total_size = 0
-status_code_counts = {str(code): 0 for code in status_codes}
-line_count = 0
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-def print_statistics():
-    print("Total file size:", total_size)
-    for code in sorted(status_code_counts.keys()):
-        count = status_code_counts[code]
-        if count > 0:
-            print(f"{code}: {count}")
 
-def process_line(line):
-    parts = line.split()
-    if len(parts) >= 7:
-        status_code = parts[-2]
-        file_size = int(parts[-1])
-        if status_code in status_code_counts:
-            status_code_counts[status_code] += 1
-            global total_size
-            total_size += file_size
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
-def signal_handler(signal, frame):
-    print_statistics()
-    sys.exit(0)
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
-# Set up the signal handler for CTRL+C
-signal.signal(signal.SIGINT, signal_handler)
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
 
-try:
-    for line in sys.stdin:
-        line = line.strip()
-        process_line(line)
-        line_count += 1
-        if line_count % 10 == 0:
-            print_statistics()
+                # File size
+                log["file_size"] += file_size
 
-except KeyboardInterrupt:
-    # Handle CTRL+C gracefully
-    signal_handler(None, None)
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
 
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
